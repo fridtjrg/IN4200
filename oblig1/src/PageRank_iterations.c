e8dd39a2efe825a53492f3dd09e204185f0cd5b0
fridtjrg <fridtjof@gjengset.com>
2022-03-25 11:02:01 +0100

    Reducing memory usage by removing x_new array

--- oblig1/src/PageRank_iterations.c
+++ oblig1/src/PageRank_iterations.c
@@ -8,7 +8,6 @@ void PageRank_iterations (int N, int *row_ptr, int *col_idx, double *val, double
 
     //initial guess
     double *x_old = malloc(N * sizeof(double));
-    double *x_new = malloc(N * sizeof(double));
     int num_W = 0; //number of dangling webpages
     int *W_webpages = malloc(N * sizeof(int));
     int *Dangling_idx= malloc(num_W* sizeof(int));
@@ -20,7 +19,6 @@ void PageRank_iterations (int N, int *row_ptr, int *col_idx, double *val, double
     #pragma omp for
     for(int i=0;i<N;i++){
         x_old[i]=1./N;
-        x_new[i]=0;
         W_webpages[i] = 0;
     }
 
@@ -78,12 +76,12 @@ void PageRank_iterations (int N, int *row_ptr, int *col_idx, double *val, double
         #pragma omp for
         for (int i=0;i<=N-2; i++){
             int n_multip = row_ptr[i+1]- row_ptr[i]-1; //nr multiplications in row
-            x_new[i] = 0;
+            scores[i] = 0;
             for (int j=0; j<=n_multip;j++){
-                x_new[i]+=val[row_ptr[i]+j]*x_old[col_idx[row_ptr[i]+j]];
+                scores[i]+=val[row_ptr[i]+j]*x_old[col_idx[row_ptr[i]+j]];
             }
 
-            x_new[i] = per_iter+ d*x_new[i];
+            scores[i] = per_iter+ d*scores[i];
 
         }
 
@@ -100,13 +98,13 @@ void PageRank_iterations (int N, int *row_ptr, int *col_idx, double *val, double
             {
                 temp+=val[row_ptr[N-1]+j]*x_old[col_idx[row_ptr[N-1]+j]];
             }
-        x_new[N-1] = per_iter+d*temp;   
+        scores[N-1] = per_iter+d*temp;   
 
         //sum scores of dandling webpages
         W=0;
         #pragma omp for reduction(+:W)
         for(int i=0;i<num_W-1;i++){
-            W+= x_new[Dangling_idx[i]];//sum scores of dangling webpages
+            W+= scores[Dangling_idx[i]];//sum scores of dangling webpages
         }
         
         test_criterion = 0;
@@ -114,24 +112,19 @@ void PageRank_iterations (int N, int *row_ptr, int *col_idx, double *val, double
         //updates old x to be new x and updates test criterion
         #pragma omp for reduction(+:test_criterion)
         for(int i=0;i<=N-1;i++){
-            test_criterion += fabs(x_new[i]-x_old[i]);
-            x_old[i] = x_new[i];
+            test_criterion += fabs(scores[i]-x_old[i]);
+            x_old[i] = scores[i];
         }
         /* For testing
         #pragma omp single
         {
-            printf("x_0= %f\n",x_new[0]);
+            printf("x_0= %f\n",scores[0]);
         }
         */
 
 }//end of iteration loop
 
 
-//filling in zeroed values
-#pragma omp for
-for(int i=0; i<=N-1;i++){
-            scores[i] = x_new[i];
-}
 
 }//end of parallell region
 
@@ -139,7 +132,7 @@ for(int i=0; i<=N-1;i++){
 //only for testing purposes
 printf("Score = \n");
 for(int i=0; i<=N-1;i++){
-            printf("%f\n",x_new[i]);
+            printf("%f\n",scores[i]);
 }
 
 
