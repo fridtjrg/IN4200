fb3585766f5e90fdd833984a430db60404b27a12
fridtjrg <fridtjof@gjengset.com>
2022-04-18 15:32:13 +0200

    Serial code working

--- Oblig2/serial_code/functions.c
+++ Oblig2/serial_code/functions.c
@@ -14,51 +14,41 @@ int n; /* # pixels in horizontal-direction */
 //to allocate?
 
 //void allocate_image(image *u, int m, int n)
-void allocate_image(*u, int m, int n){
+void allocate_image(image *u, int m, int n){
+    
+    u->m = m; u->n =n;
 
-	u.image_data[m];
+	u->image_data = (float**)malloc(m*sizeof(float*)); 
     for (int i = 0; i < m; i++)
-        u.image_data[i] = (int*)malloc(n * sizeof(int));
- 
-
-    int count = 0;
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
-            u.image_data[i][j] = ++count;
- 
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
-            printf("%d ", u.image_data[i][j]);
-
- 
-    return 0;
+        u->image_data[i] = (float*)malloc(n * sizeof(float));
 }
+    
 
-//How am i suppose to allocate 2D without m?
 
 //void deallocate_image(image *u)
-void deallocate_image(*u){
-	for (int i = 0; i < u.m; i++)
-        free(u.image_data[i]);
+void deallocate_image(image *u){
+	for (int i = 0; i < u->m; i++)
+        free(u->image_data[i]);
 
-    //if 1D just free(u)
+    free(u->image_data);
 }
 
 void convert_jpeg_to_image(const unsigned char* image_chars, image *u){
 //convert a 1D array of unsigned char values into an image struct
 
 //NB: Assuming 1st element is top left and reading row-wise
-    for (int i = 0; i < u.m; i++){
-        for(int j=0; j < u.n; j++)
-            u.image_data[i][j] = atof(image_chars[i*u.m+j]);
+    for(int i = 0; i < u->m; i++){
+        for(int j=0; j < u->n; j++){
+            u->image_data[i][j] = (float)image_chars[i*u->n+j];
+        }
     }
 
 }
 
 void convert_image_to_jpeg(const image *u, unsigned char* image_chars){
-    for(int i = 0; i < u.m; i++){
-        for(int j=0; j < u.n; j++){
-            gcvt(u.image_data[i][j], 6, image_chars[i*u.m+j]);//copies and transforms to char
+    for(int i = 0; i < u->m; i++){
+        for(int j=0; j < u->n; j++){
+            image_chars[i*u->n+j] =(char)u->image_data[i][j];
         }
     }	
 }
@@ -68,9 +58,9 @@ void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters){
     for(int iter=0; iter<iters; iter++){
 
         //does not include the outer bounds
-        for(int i = 1; i < u.m-1; i++){
-            for(int j=1; j < u.n-1; j++){
-                u_bar.image_data[i][j] = u.image_data[i][j] + kappa*(u.image_data[i-1][j] + u.image_data[i][j-1]-4*u.image_data[i][j] +u.image_data[i+1][j]+u.image_data[i][j+1]);
+        for(int i = 1; i < u->m-1; i++){
+            for(int j=1; j < u->n-1; j++){
+                u_bar->image_data[i][j] = u->image_data[i][j] + kappa*(u->image_data[i-1][j] + u->image_data[i][j-1]-4*u->image_data[i][j] +u->image_data[i+1][j]+u->image_data[i][j+1]);
             }
         }
 
@@ -78,7 +68,7 @@ void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters){
         if(iter!=iters-1){
             image temp = *u;
             *u = *u_bar;
-            *u_bar = *u;
+            *u_bar = temp;
         }
 
     }//end of iteration loop
--- Oblig2/serial_code/serial_main.c
+++ Oblig2/serial_code/serial_main.c
@@ -1,13 +1,8 @@
-/* needed header files .... */
+#include "serial_main.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct{
-float** image_data; /* a 2D array of floats */
-int m; /* # pixels in vertical-direction */
-int n; /* # pixels in horizontal-direction */
-}
-image;
+
 
 /* declarations of functions import_JPEG_file and export_JPEG_file */
 void import_JPEG_file (const char* filename, unsigned char** image_chars,int* image_height,int* image_width,int* num_components);
@@ -35,28 +30,28 @@ int main(int argc, char *argv[])
 		printf("using default values!\n");
 
 		kappa = 0.1; //0.2 or lower
-		iters= 1000; //Many?
-		input_jpeg_filename= "mona_lisa_noisy.jpg";
-		output_jpeg_filename = "mona_lisa_processed.jpg";
+		iters= 100; //Many?
+		input_jpeg_filename= "datafiles/mona_lisa_noisy.jpg";
+		output_jpeg_filename = "datafiles/mona_lisa_serial_processed.jpg";
 	}
 	/* read from command line: kappa, iters, input_jpeg_filename, output_jpeg_filename */
 	/* ... */
 
-	//Updating structs
-	u.m = m;
-	u.n =n;
-	u_bar.m = m;
-	u_bar.n = n;
+
 
 	import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
 
+
 	allocate_image(&u, m, n);
 	allocate_image(&u_bar, m, n);
+
 	convert_jpeg_to_image(image_chars, &u);
+
 
 	iso_diffusion_denoising (&u, &u_bar, kappa, iters);
 
 	convert_image_to_jpeg (&u_bar, image_chars);
+
 	export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
 
 	deallocate_image (&u);
