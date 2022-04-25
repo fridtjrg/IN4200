e8a765502d2ccbc2499f6f49b410cd18ddb9ba72
fridtjrg <fridtjof@gjengset.com>
2022-04-25 12:34:43 +0200

    u

--- Oblig2/parallel_code/main_parallel.c
+++ Oblig2/parallel_code/main_parallel.c
@@ -111,7 +111,7 @@ int main(int argc, char *argv[])
 	if (my_rank==0) {
 		//printf("Printing imagechars:\n");
 	    //for(int i = 0; i < m*n; i++){
-            	printf("Index = %d, data= %d \n",i,image_chars[i]);
+            	//printf("Index = %d, data= %d \n",i,image_chars[i]);
 	    //}	
 		convert_image_to_jpeg(&whole_image, image_chars);
 		export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
