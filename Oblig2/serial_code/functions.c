1e9ca078b3fba12fcf45132ac95425b34db64920
fridtjrg <fridtjof@gjengset.com>
2022-04-29 17:21:20 +0200

    Updates serial function.c to match that of parallel but without mpi

--- Oblig2/serial_code/functions.c
+++ Oblig2/serial_code/functions.c
@@ -1,3 +1,4 @@
+//Serial functions
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,13 +10,9 @@ int n; /* # pixels in horizontal-direction */
 }image;
 
 
-//Does it have to be a 2D array or can i just use..
-//u = (float*)malloc(n*m*sizeof(float));
-//to allocate?
 
-//void allocate_image(image *u, int m, int n)
+//Accocates image from struct
 void allocate_image(image *u, int m, int n){
-    
     u->m = m; u->n =n;
 
     u->image_data = (float**)malloc(m*n*sizeof(float*)); //was originally just m
@@ -25,56 +22,54 @@ void allocate_image(image *u, int m, int n){
 }
     
 
-
-//void deallocate_image(image *u)
+//Frees image from struct
 void deallocate_image(image *u){
-	for (int i = 0; i < u->m; i++)
+    for (int i = 0; i < u->m; i++)
         free(u->image_data[i]);
 
     free(u->image_data);
 }
 
+//Assigns 1d image_chars array to a 2D float array in struct
 void convert_jpeg_to_image(const unsigned char* image_chars, image *u){
-//convert a 1D array of unsigned char values into an image struct
-
-//NB: Assuming 1st element is top left and reading row-wise
     for(int i = 0; i < u->m; i++){
         for(int j=0; j < u->n; j++){
             u->image_data[i][j] = (float)image_chars[i*u->n+j];
         }
     }
-
 }
 
+//Assigns values from 2d float array in struct to 1d char array
 void convert_image_to_jpeg(const image *u, unsigned char* image_chars){
     for(int i = 0; i < u->m; i++){
         for(int j=0; j < u->n; j++){
             image_chars[i*u->n+j] =(char)u->image_data[i][j];
         }
-    }	
+    }   
 }
 
+//Performs ISO diffusion denoising
 void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters){
 
     for(int iter=0; iter<iters; iter++){
 
-        //does not include the outer bounds
+        //As loop does not contain outer bound points
+        //they should remain constant.
         for(int i = 1; i < u->m-1; i++){
             for(int j=1; j < u->n-1; j++){
                 u_bar->image_data[i][j] = u->image_data[i][j] + kappa*(u->image_data[i-1][j] + u->image_data[i][j-1]-4*u->image_data[i][j] +u->image_data[i+1][j]+u->image_data[i][j+1]);
             }
         }
 
-        //Swaps pointers only if it is not the last iteration
+        //Swaps pointers only if it is NOT the last iteration
         if(iter!=iters-1){
             image temp = *u;
             *u = *u_bar;
             *u_bar = temp;
         }
-
-    }//end of iteration loop
-
-
+    }
 }
+
+
 
 
