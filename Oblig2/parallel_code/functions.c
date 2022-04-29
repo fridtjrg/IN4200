c8bff00066edd23cd9772798e998a54d78670a98
fridtjrg <fridtjof@gjengset.com>
2022-04-29 15:34:16 +0200

    u

--- Oblig2/parallel_code/functions.c
+++ Oblig2/parallel_code/functions.c
@@ -20,7 +20,7 @@ void allocate_image(image *u, int m, int n){
     
     u->m = m; u->n =n;
 
-	u->image_data = (float**)malloc(m*n*sizeof(float*)); //was originally just m
+	u->image_data = (float**)malloc(m*sizeof(float*)); //was originally just m
     for (int i = 0; i < m; i++)
         u->image_data[i] = (float*)malloc(n * sizeof(float));
 }
