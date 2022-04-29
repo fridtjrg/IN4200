94714c26ab0a77d453041823557d4958c2a6a9a8
fridtjrg <fridtjof@gjengset.com>
2022-04-29 15:50:22 +0200

    u

--- Oblig2/parallel_code/main_parallel.c
+++ Oblig2/parallel_code/main_parallel.c
@@ -99,9 +99,10 @@ int main(int argc, char *argv[])
 	//all processes must be done before collecting results
 	MPI_Barrier(MPI_COMM_WORLD);
 
+	int *image_chunks = malloc(num_procs*sizeof(int));
 
 	//no longer using gatherv or index skip
-	MPI_Gather(u.image_data, process_chunk_size, MPI_FLOAT, whole_image.image_data, m*n, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	MPI_Gatherv(u.image_data, process_chunk_size, MPI_FLOAT, whole_image.image_data, m*n, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
 	//Process one must have obtained the entire image_chars array
 	MPI_Barrier(MPI_COMM_WORLD);
@@ -117,6 +118,7 @@ int main(int argc, char *argv[])
 	
 	deallocate_image(&u);
 	deallocate_image(&u_bar);
+    free(image_chunks);
     free(my_image_chars);
 	MPI_Finalize ();
 	return 0;
