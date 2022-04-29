7b53e8c3f0ce0a109a8818e3b54cb36577ddcfbc
fridtjrg <fridtjof@gjengset.com>
2022-04-29 17:23:42 +0200

    Moves mpi include to headerfile

--- Oblig2/parallel_code/main_parallel.c
+++ Oblig2/parallel_code/main_parallel.c
@@ -1,5 +1,4 @@
 #include "main_parallel.h"
-#include <mpi.h>
 
 void import_JPEG_file (const char* filename, unsigned char** image_chars,int* image_height,int* image_width,int* num_components);
 void export_JPEG_file (const char* filename, const unsigned char* image_chars,int image_height, int image_width,int num_components, int quality);
