81f17a141d515fe1143f9f5154afb8446653a481
fridtjrg <fridtjof@gjengset.com>
2022-04-29 16:23:53 +0200

    u

--- Oblig2/parallel_code/main_parallel.c
+++ Oblig2/parallel_code/main_parallel.c
@@ -111,14 +111,13 @@ int main(int argc, char *argv[])
 	MPI_Barrier(MPI_COMM_WORLD);
 
 
-
-	if (my_rank==0) {
+	if(my_rank==0) {
 		convert_image_to_jpeg(&whole_image, image_chars);
 		export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
 		deallocate_image(&whole_image);
 	}
 
-	
+	if(my_rank!=0){deallocate_image(&u);}
 	//deallocate_image(&u);
 	deallocate_image(&u_bar);
     free(my_image_chars);
