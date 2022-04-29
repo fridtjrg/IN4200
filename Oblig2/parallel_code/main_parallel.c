b878b38409af7f91e948b948c3f783c0651a6598
fridtjrg <fridtjof@gjengset.com>
2022-04-29 17:16:00 +0200

    cleaning main_parallel

--- Oblig2/parallel_code/main_parallel.c
+++ Oblig2/parallel_code/main_parallel.c
@@ -79,51 +79,54 @@ int main(int argc, char *argv[])
 	MPI_Gather(&my_row_skip, 1, MPI_INT, &process_row_skips[my_rank], 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 
-
 	//Contains the index of each chunks first element in image_chars array
 	int *chunk_idx = malloc(num_procs*sizeof(*chunk_idx));
     for(int proc = 0; proc < num_procs; proc++){
         chunk_idx[proc] = proc*process_chunk_sizes[proc];
     }
 
+    //Each process recives it's part of the image(image_chars)
     unsigned char *my_image_chars = malloc(process_chunk_size*sizeof(my_image_chars));
-	//MPI(Send data, How many to send of type, type, Recive databuffer, count, type, root, communicator)
 	MPI_Scatterv(image_chars, process_chunk_sizes, chunk_idx, MPI_UNSIGNED_CHAR, my_image_chars, process_chunk_size, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+	free(process_chunk_sizes);
+	free(chunk_idx);
 
 
-
+	//Allocates memory for this processes' calculation
     allocate_image(&u, my_m, my_n);
     allocate_image(&u_bar, my_m, my_n);
 
-
-
+    //Converts 1d char array to struct
 	convert_jpeg_to_image(my_image_chars, &u);
+	free(my_image_chars);
 
+	//Performs ISO diffusion denoising in parallel
 	iso_diffusion_denoising_parallel(&u, &u_bar, kappa, iters, my_rank, num_procs);
-
 
 	//all processes must be done before collecting results
 	MPI_Barrier(MPI_COMM_WORLD);
 
 	int total_datapoints = m*n;
-	//no longer using gatherv or index skip
+	
+	//The results of each process is gathered and stored in whole_image by process 0
 	MPI_Gatherv(u.image_data, process_chunk_size, MPI_FLOAT, whole_image.image_data, &total_datapoints,process_row_skips, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	free(process_row_skips);
 
-	//Process one must have obtained the entire image_chars array
+	//Process 0 must have obtained the entire image_chars array
 	MPI_Barrier(MPI_COMM_WORLD);
 
-
+	//process 0 creates and saves image, and deallocates whole_image
 	if(my_rank==0) {
 		convert_image_to_jpeg(&whole_image, image_chars);
 		export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
 		deallocate_image(&whole_image);
 	}
 
+	//Process 0 can not deallocate u as it has the same adress as whole_image
 	if(my_rank!=0){deallocate_image(&u);}
-	deallocate_image(&u_bar);
-    free(my_image_chars);
 
-    MPI_Barrier(MPI_COMM_WORLD);
+	deallocate_image(&u_bar);
+   
 	MPI_Finalize ();
 	return 0;
 }
