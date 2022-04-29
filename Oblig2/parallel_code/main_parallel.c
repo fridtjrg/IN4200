1ea8e192af6fab0c06552b6c4a346b0fa0e92663
fridtjrg <fridtjof@gjengset.com>
2022-04-29 16:16:51 +0200

    u

--- Oblig2/parallel_code/main_parallel.c
+++ Oblig2/parallel_code/main_parallel.c
@@ -115,7 +115,7 @@ int main(int argc, char *argv[])
 	if (my_rank==0) {
 		convert_image_to_jpeg(&whole_image, image_chars);
 		export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
-		deallocate_image (&whole_image);
+		deallocate_image(whole_image);
 	}
 
 	
